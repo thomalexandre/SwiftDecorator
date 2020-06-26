7090c93154d3f9682ed4b4e60d24a5a883fc4fd9
Alexandre Thomas <alexandre.thomas@accedo.tv>
2020-06-26 13:45:15 +0200

    project arrangement

---
+++ SwiftDecoratorTests/MissingKeyTextProviderTests.swift
@@ -0,0 +1,28 @@
+//
+//  MissingKeyTextProviderTests.swift
+//  SwiftDecoratorTests
+//
+//  Created by Alexandre Thomas on 2020. 06. 26..
+//  Copyright © 2020. Alexandre Thomas. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import SwiftDecorator
+
+class MissingKeyTextProviderTests: XCTestCase {
+
+    func testLocalise() {
+        
+        // TODO
+    }
+
+}
+
+
+//extension MissingKeyTextProviderTests: TextProvider {
+//    
+//    func localise(_ key: String) -> String {
+//        return key.elementsEqual("platform") ? "ios" : key
+//    }
+//}
